e7bb970d5c37c7cf3b4c89716418cb986900ea71
Kyounghan Ryu <thankryu@gmail.com>
2023-04-20 17:34:14 +0900

    feat : 회원 repository 추가

---
+++ src/main/java/com/thankryu/trip/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.thankryu.trip.repository;
+
+import com.thankryu.trip.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+}
